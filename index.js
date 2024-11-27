f4136f921564905b5e76c2933bfe9d60042b1005
Lucas Vasconcelos <lucasvasconcelospiranicarneiro@gmail.com>
2024-11-27 19:46:19 +0000

    Conectando a um  Banco de Dados teste pelo MongoDB

--- index.js
+++ index.js
@@ -1,213 +1,210 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 /************************************************/
-/*const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 var cors = require('cors');
-mongoose.connect('mongodb+srv://vaguetti:dwm20221@cluster0.004qy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-     useNewUrlParser: true,
-     useUnifiedTopology: true
-});*/
-/******************************************/
+mongoose.connect('mongodb+srv://lucasvaspicarneiro:%40Lucas130205@cluster0.ge3mv.mongodb.net/teste?retryWrites=true&w=majority')
+.then(() => {
+    console.log("Conexão com o MongoDB foi bem-sucedida!");
+})
+.catch((error) => {
+    console.error("Erro ao conectar ao MongoDB:", error);
+});
+/******************************************************/
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-/***************************************************/
-
-// Rota Básica
-/*app.get("/ifpets/", (req, res) => {
-     
-});*/
+app.use(express.static('public'));
+/******************************************************/
 
 /*Rotas HTTP*/
 
 /* Cadastro de Produtos */
 // Adicionar um novo produto
-app.post("/ifpets/product", (req, res) => {
+app.post("/product", (req, res) => {
      
 });
 
 // Retorna todos os produtos
-app.get("/ifpets/product", (req, res) => {
+app.get("/product", (req, res) => {
 
 });
 
 // exclui produto
-app.delete("/ifpets/product/:id", (req, res) => {
+app.delete("/product/:id", (req, res) => {
 
 });
 
 // alterar produto
-app.put("/ifpets/product/:id", (req,res) => {
+app.put("/product/:id", (req,res) => {
      
 });
 
 /* Cadastro de Serviços */
 // Adicionar um novo Servico
-app.post("/ifpets/service", (req, res) => {
+app.post("/service", (req, res) => {
 
 });
 
 // Retorna todos os serviços
-app.get("/ifpets/service", (req, res) => {
+app.get("/service", (req, res) => {
 
 });
 
 // exclui serviço
-app.delete("/ifpets/service/:id", (req, res) => {
+app.delete("/service/:id", (req, res) => {
 
 });
 
 // alterar serviço
-app.put("/ifpets/service/:id", (req,res) => {
+app.put("/service/:id", (req,res) => {
 
 });
 
 /*****************************************************/
 
 /* Apresentação */
-app.get("/ifpets/apresentacao", (req, res) => {
+app.get("/", (req, res) => {
     res.send("Apresentacao Loja");
      // Landing Page
 });
 
 /* Principal */
-app.get("/ifpets/principal", (req, res) => {
-     // retorna todos os tipos de produtos e servicos     
+app.get("/principal", (req, res) => {
+     // retorna todos os tipos de produtos e servicos
+     res.send("Pagina principal da Loja");
 });
 
-// Busca ???
-
 /* Cadastro Usuário */
-app.post("/ifpets/user", (req, res) => {
+app.post("/user", (req, res) => {
      
 });
 
-app.post("/ifpets/user/address", (req, res) => {
+app.post("/user/address", (req, res) => {
 
 });
 
 /* Login - Usuário */
-app.post("/ifpets/user/login", (req, res) => {
+app.post("/user/login", (req, res) => {
 
 });
 
 /* Produtos Favoritos */
-app.get("/ifpets/user/favoritos", (req, res) => {
+app.get("/user/favoritos", (req, res) => {
      res.send("Retorna os produtos favoritos");
 });
 
 // Remove o produto da lista de favoritos do usuário 
-app.delete("/ifpets/user/favoritos/products/:id", (req, res) => {
+app.delete("/user/favoritos/products/:id", (req, res) => {
      
 });
 
 // Alterar se o produto ainda é o favorito
-app.put("/ifpets/user/favoritos/products/:id", (req, res) => {
+app.put("/user/favoritos/products/:id", (req, res) => {
      
 });
 
 /* Sacola de Compras */
 // Retorna a lista dos itens no carrinho de compras do usuário
-app.get("/ifpets/user/carcompras", (req, res) => {
-     
+app.get("/user/carcompras", (req, res) => {
+     res.send("Lista de Itens no carrinho");
 });
 
 // deleta um produto do carrinho de compras
-app.delete("/ifpets/user/carcompras/:id", (req, res) => {
+app.delete("/user/carcompras/:id", (req, res) => {
 
 });
 
 // Modifica a quantidade dos itens do carrinho de compra
-app.put("/ifpets/user/carcompras/:id", (req, res) => {
+app.put("/user/carcompras/:id", (req, res) => {
 
 });
 
 /* Produtos Cães */
 // Landing Page dos Tipos de Produtos para cachorro
-app.get("/ifpets/produtos/buscarproduto/dogs", (req, res) => {
+app.get("/produtos/buscarproduto/dogs", (req, res) => {
 
 });
 
 // Lista os Produtos filtrando só os de cachorros e destinado a higiene
-app.get("/ifpets/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=higiene", (req, res) => {
+app.get("/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=higiene", (req, res) => {
      
 });
 
 // Lista os Produtos filtrando só os de cachorros e destinado a racao
-app.get("/ifpets/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=racao", (req, res) => {
+app.get("/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=racao", (req, res) => {
 
 });
 
 // Lista os Produtos filtrando só os de cachorros e destinado a acessorios
-app.get("/ifpets/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=acessorio", (req, res) => {
+app.get("/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=acessorio", (req, res) => {
 
 });
 
 // Lista os Produtos filtrando só os de cachorros e destinado a medicamentos
-app.get("/ifpets/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=medicamento", (req, res) => {
+app.get("/produtos/buscarproduto/dogs/?tipoProduct=cachorro/?categoriaProduct=medicamento", (req, res) => {
 
 });
 
 /* Produtos Gatos */
 // Landing Page dos Tipos de Produtos para gatos
-app.get("/ifpets/produtos/buscarproduto/cats", (req, res) => {
+app.get("/produtos/buscarproduto/cats", (req, res) => {
 
 });
 
 // Lista os Produtos filtrando só os de gatos e destinado a higiene
-app.get("/ifpets/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=higiene", (req, res) => {
+app.get("/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=higiene", (req, res) => {
 
 });
 
 // Lista os Produtos filtrando só os de gatos e destinado a racao
-app.get("/ifpets/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=racao", (req, res) => {
+app.get("/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=racao", (req, res) => {
 
 });
 
 // Lista os Produtos filtrando só os de gatos e destinado a acessorios
-app.get("/ifpets/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=acessorio", (req, res) => {
+app.get("/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=acessorio", (req, res) => {
 
 });
 
 // Lista os Produtos filtrando só os de gatos e destinado a medicamentos
-app.get("/ifpets/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=medicamento", (req, res) => {
+app.get("/produtos/buscarproduto/cats/?tipoProduct=gato/?categoriaProduct=medicamento", (req, res) => {
 
 });
 
 /* Serviços */
-app.get("/ifpets/servicos/buscarservico/?tipoService=vacinacao", (req, res) => {
+app.get("/servicos/buscarservico/?tipoService=vacinacao", (req, res) => {
      // Landing Page
 });
 
-app.get("/ifpets/servicos/buscarservico/?tipoService=banho_tossa", (req, res) => {
+app.get("/servicos/buscarservico/?tipoService=banho_tossa", (req, res) => {
      // Landing Page
 });
 
-app.get("/ifpets/servicos/buscarservico/?tipoService=adestramento", (req, res) => {
+app.get("/servicos/buscarservico/?tipoService=adestramento", (req, res) => {
      // Landing Page
 });
 
-app.post("/ifpets/servicos/buscarservico/?tipoService=vacinacao/agendamento", (req, res) => {
+app.post("/servicos/buscarservico/?tipoService=vacinacao/agendamento", (req, res) => {
      
 });
 
-app.post("/ifpets/servicos/buscarservico/?tipoService=banho_tossa/agendamento", (req, res) => {
+app.post("/servicos/buscarservico/?tipoService=banho_tossa/agendamento", (req, res) => {
      
 });
 
-app.post("/ifpets/servicos/buscarservico/?tipoService=adestramento/agendamento", (req, res) => {
+app.post("/servicos/buscarservico/?tipoService=adestramento/agendamento", (req, res) => {
      
 });
 
 /* Validação da Compra */
 // Retorna o total a pagar possibilitando a compra
-app.get("/ifpets/validarcomprar", (req, res) => {
+app.get("/validarcomprar", (req, res) => {
 
 });
 
 /* Usuário escolhe as formas de pagamento*/
-app.post("/ifpets/validarcomprar", (req, res) => {
+app.post("/validarcomprar", (req, res) => {
 
 });
 
